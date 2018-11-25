c755cdde59db9d808a057f91297be065526d04af
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-11-26 02:37:10 +0600

    Added redux, redux-ducks

---
+++ src/ducks/sortableTable/index.js
@@ -0,0 +1,25 @@
+// Actions
+const LOAD   = 'app/sortable-table/LOAD';
+const CREATE = 'app/sortable-table/CREATE';
+const UPDATE = 'app/sortable-table/UPDATE';
+const REMOVE = 'app/sortable-table/REMOVE';
+
+// Reducer
+export default function reducer(state = {"1":"Hello world"}, action = {}) {
+  switch (action.type) {
+    // do reducer stuff
+    default: return state;
+  }
+}
+
+// Action Creators
+export function loadUsers() {
+  return { type: LOAD };
+}
+
+// side effects, only as applicable
+// e.g. thunks, epics, etc
+export function getUsers () {
+  console.log("ok")
+  //return dispatch => get('/widget').then(widget => dispatch(updateWidget(widget)))
+}
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import sortableTable from '../../ducks/sortableTable';
+
+const rootReducer = combineReducers({
+    sortableTable
+});
+export default rootReducer;

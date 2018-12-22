81967d211825f09ce6caca662668d405c6829c4f
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-12-22 20:50:55 +0600

    Fixed App.test.js logic

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
+import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
